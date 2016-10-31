72bbd70be3347c136ef932a809d841739f0f27ee
katya20 <kpanchenko21@gmail.com>
2016-10-31 09:01:33 -0700

    Classadoo autocommit

--- page.js
+++ page.js
@@ -28,3 +28,28 @@ nameInput.keydown(function(e) {
   }	
 })
 
+// To Do List (today)
+
+var input1 = $("#checkbox1")
+
+
+
+input1.keydown(function(e) {	
+  if (e.keyCode == "13") {
+    addListItem(nameInput.val() + "/1", input1.val()); 
+    console.log("input1 keydown is working");
+    $("#output1").append(input1.val());
+  }
+})
+
+onNewListItem(nameInput.val() + "1", function(value) {
+  $("#output1").append("<div>" + value + "</div>")
+  console.log(value);
+}) 
+
+$("#clear-button-1").click(function() {
+  deleteValue(nameInput.val() + "1")
+  $("#output1").html("");
+});
+
+
