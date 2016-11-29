c04e8f52263c6a02dac194757a6bc4290368e9a5
katya20 <kpanchenko21@gmail.com>
2016-11-29 09:21:45 -0800

    Classadoo autocommit

--- firebase-config.js
+++ firebase-config.js
@@ -37,14 +37,14 @@ function onNewValue(name, callback) {
 function getNotesForName1(name) {  
   console.log("getNotesForName1")
   onNewListItem(nameInput.val() + "/1", function(note) {
-    $("#output1").append("<div>" + note + "</div>")
+    $(".output1").append("<div>" + note + "</div>")
   })
 }
 
 function getNotesForName2(name) {  
   console.log("getNotesForName2")
   onNewListItem(nameInput.val() + "/2", function(note) {
-    $("#output2").append("<div>" + note + "</div>")
+    $(".output2").append("<div>" + note + "</div>")
   })
 }
 
