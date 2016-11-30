253ed3f0760c0ac6212ccbdf43aecf67cc4ad7b2
katya20 <kpanchenko21@gmail.com>
2016-11-30 09:21:38 -0800

    Classadoo autocommit

--- page.js
+++ page.js
@@ -19,6 +19,14 @@ function checkTime(i) {
 var nameInput = $("#nameInput");
 
 nameInput.keydown(function(e) {
+  function myFunction() {
+    var person = prompt("Please enter your name", "Harry Potter");
+
+    if (person != null) {
+      document.getElementById("demo").innerHTML =
+        "Hello " + person + "! How are you today?";
+    }
+  }
   if (e.which == 13) {
     getNotesForName1(nameInput.val());
     getNotesForName2(nameInput.val());
