5180edc413dd5b4f1e3c7654e422a12cee6b916f
katya20 <kpanchenko21@gmail.com>
2016-12-14 09:51:53 -0800

    Classadoo autocommit

---
+++ firebase-config.js
@@ -0,0 +1,85 @@
+var firebaseConfig = {
+  apiKey: " AIzaSyD-U1vfBg6sjZdxjgzgNzB97dfFvNrcX38",
+  authDomain: "nueva-project-1.firebaseapp.com",
+  databaseURL: "https://nueva-project-1.firebaseio.com"	
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var store = firebase.database().ref("challenge-2")
+
+
+
+function popUp() {
+  var person = prompt("Please enter your name to save your data :)", "Harry Potter");
+  console.log("what?", person)
+  return person
+}
+
+
+function newName(nameInput) {
+  console.log("persons",nameInput)
+  person = nameInput
+  getNotesForName1(nameInput);
+  getNotesForName2(nameInput);
+  console.log(nameInput);
+  $("#output1").html("");
+  $("#output2").html("");
+  $("#greeting").append("Hello, " + nameInput);
+  $("#nameInput").css({display: "none"});
+}
+
+
+function storeValue(name, value) {	
+  var obj = {}
+  obj[name] = value;
+  store.child("vars").update(obj)
+}
+
+function addListItem(name, value) {		
+  store.child("vars" + "/" + name).push(value)
+}
+
+function onNewListItem(name, callback) {		
+  store.child("vars" + "/" + name).on("child_added", function(data) {
+//    console.log(data.key)
+    callback(data.val(), data.key)
+  })
+}
+//name+ "/1/" + id +"/"
+function deleteValue(name) {	
+  store.child("vars" + "/" + name).remove()
+}
+
+
+
+function onNewValue(name, callback) {	
+  store.child("vars").child(name).on("value", function(data) {		
+    callback(data.val());
+  })
+}
+
+function getNotesForName1(name) {  
+  console.log("getNotesForName1")
+  onNewListItem(name + "/1", function(note, id) {
+    var el = $("<div>" + note + "</div>")
+    $(".output1").append(el)
+    el.click(function() {
+      el.remove()  
+      deleteValue(name + "/" + "/1/" + id)
+      //delet note.id from firebase
+    })
+  })
+}
+
+
+function getNotesForName2(name) {  
+  console.log("getNotesForName2")
+  onNewListItem(name + "/2", function(note) {
+    $(".output2").append("<div>" + note + "</div>")
+  })
+}
+
+
+
+
---
+++ page.js
@@ -0,0 +1,64 @@
+// Clock & time refresh
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  m = checkTime(m);
+  $('#clock').html(h + ":" + m);
+  var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  
+  return i;
+}
+
+// Name
+
+var nameInput = $("#nameInput");
+
+nameInput.keydown(function(e) {
+  if (e.which == 13) {
+    newName(nameInput.val());
+    e.preventDefault();
+  }	 
+})
+
+
+
+// To Do List (today)
+
+var input1 = $("#checkbox1")
+
+
+var person;
+input1.keydown(function(e) {
+  if (e.keyCode == "13") {
+    if (!person) {
+      var newPerson = popUp();
+      
+      if (newPerson != null) {   
+        newName(newPerson);  
+        console.log("personsss", person)
+        addListItem(person + "/1", input1.val()); 
+      }            
+    } else {
+      addListItem(person + "/1", input1.val()); 
+    }
+    input1.val('');
+//    $("#output1").append(input1.val());
+  }
+})
+
+
+$("#clear-button-1").click(function() {
+  deleteValue(nameInput.val() + "/1")
+  $(".output1").html("");
+});
+
+$(".output1").click(function() {
+  console.log("output1 clicked");
+});
+
+
